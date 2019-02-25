2373d6055978ffbdf5e2fed0388024dd2b009661
Abhilash <Abhilash@gmail.com>
2019-02-25 15:29:08 +0000

    new update

--- hello.java
+++ hello.java
@@ -1,10 +1,12 @@
 class hello{  
  public static void main(String args[]){  
   int i,fact=1;  
-  int number=5;//It is the number to calculate factorial    
+  int number=6;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
+  System.out.println("Factorial of "+number+" is: "+fact);   
+ 
   System.out.println("Factorial of "+number+" is: "+fact);    
  }  
 }  
